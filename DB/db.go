b1c1f01c4b9827997d9c5c3b2b34e2c6ca4f18b0
prasanna kanagasabai <prsanna.in@gmail.com>
2016-12-29 12:41:56 +0800

    pra

--- DB/db.go
+++ DB/db.go
@@ -63,18 +63,18 @@ func (db *DB) GetHosts(username string)([]Host){
 
 func Setup(db *DB) {
 	//db.DropTableIfExists(User{},Host{})
-	//db.CreateTable(User{},Host{})
-	user := User{Username:"Admin",Password:"Password"}
+	db.CreateTable(User{},Host{})
+//	user := User{Username:"Admin",Password:"Password"}
 	//db.Create(&user)
-	var h Host
-	var u User
-	host :=Host{Hostname:"https://google.com",LastScan:time.Now(),NextScan:time.Now()}
-	e := db.Create(&host).First(&user).Error
-	if e != nil{
-		log.Fatal(e)
-	}
-	db.First(&h)
-	db.First(&u)
-	log.Print("PK",h)
-	log.Print("JK",u)
+	//var h Host
+	//var u User
+	//host :=Host{Hostname:"https://google.com",LastScan:time.Now(),NextScan:time.Now()}
+	//e := db.Create(&host).First(&user).Error
+	//if e != nil{
+	//	log.Fatal(e)
+	//}
+	//db.First(&h)
+	//db.First(&u)
+	//log.Print("PK",h)
+	//log.Print("JK",u)
 }
