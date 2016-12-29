bf75ba06892031c7c52feb9e56aea5e6e5d10a25
prasanna kanagasabai <prsanna.in@gmail.com>
2016-12-29 18:47:40 +0800

    Adding Host Support

--- DB/DbManager.go
+++ DB/DbManager.go
@@ -4,6 +4,9 @@ package DB
 type DbManager interface {
 	Close() error
 	Login(string,string) *User
+	GetUser(string)*User
 	GetHosts(string) ([]Host)
 	GetHost(int) Host
+	GetScans(int)[]Scan
+	GetScan(int) Scan
 }
