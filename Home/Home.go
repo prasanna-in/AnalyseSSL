ff57ab33ce4bcfff0baef2a201a36919de09c59e
prasanna kanagasabai <prsanna.in@gmail.com>
2017-01-02 18:39:36 +0800

    Create Hosts in API

--- Home/Home.go
+++ Home/Home.go
@@ -83,7 +83,7 @@ func handleScan(sess *sessions.CookieStore,db DB.DbManager)http.Handler  {
 		}
 		detailedinfo,_ := progress.DetailedInfo(info.Endpoints[0].IPAdress)
 		details := detailedinfo.Details
-		fmt.Fprintf(resp,details.HTTPStatusCode)
+		fmt.Fprintf(resp,fmt.Sprint(details.DrownVulnerable))
 
 	})
 
