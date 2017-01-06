275a6c10a3f458e206ccb980d8428ac4e76c2fed
prasanna kanagasabai <prsanna.in@gmail.com>
2017-01-06 15:34:59 +0800

    Added Hostname Support

--- Home/Home.go
+++ Home/Home.go
@@ -113,7 +113,7 @@ func handleHost(jar *sessions.CookieStore, db DB.DbManager) http.Handler {
 		header = append(header,"Poodle TLS")
 		header = append(header,"Key Size")
 		header = append(header,"Key Strength")
-		header = append(header."Key Sign Algorithm")
+		header = append(header,"Key Sign Algorithm")
 		record = append(record,header)
 		totalHosts:=0
 		for _, value := range scans {
