4d5e1162e5774e8af9c743cc3b2790c9231c86f7
prasanna kanagasabai <prsanna.in@gmail.com>
2017-01-06 16:10:38 +0800

    Added Hostname Support

--- DB/db.go
+++ DB/db.go
@@ -156,7 +156,7 @@ func Setup(db *DB) {
 	u := User{}
 	db.Where("Username=?","Admin2").First(&u)
 	host := Host{
-		Hostname:"edit.business.sc.com",
+		Hostname:"research.standarscharted.com",
 		UserID:u.ID,
 	}
 	db.Create(&host)
