a88e4e239f8b2300b5d9519ec7cd33c0a191d5d7
prasanna kanagasabai <prsanna.in@gmail.com>
2017-01-06 15:39:02 +0800

    Added Hostname Support

--- DB/db.go
+++ DB/db.go
@@ -156,7 +156,7 @@ func Setup(db *DB) {
 	u := User{}
 	db.Where("Username=?","Admin2").First(&u)
 	host := Host{
-		Hostname:"b2bexsso.standardchartered.com",
+		Hostname:"s2b.standardchartered.com",
 		UserID:u.ID,
 	}
 	db.Create(&host)
